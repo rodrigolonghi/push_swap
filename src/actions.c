9cb791f80f6065e3ffbf21fac810a25a41a5cea0
Rodrigo Longhi <rodrigo.longhi@mercadolivre.com>
2022-04-03 00:18:00 -0300

    Ajustes nas actions

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:19:15 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/11/10 00:32:17 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2022/04/03 00:13:42 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,25 @@ void	action_pa(t_stacks *s)
 	int	x;
 	int	aux;
 
-	x = 0;
-	aux = s->stack_b[0];
-	while (x < get_relative_size(s->stack_b, s->size) - 1)
+	if (get_relative_size(s->stack_b, s->size) > 0)
 	{
-		s->stack_b[x] = s->stack_b[x + 1];
-		x++;
+		x = 0;
+		aux = s->stack_b[0];
+		while (x < get_relative_size(s->stack_b, s->size) - 1)
+		{
+			s->stack_b[x] = s->stack_b[x + 1];
+			x++;
+		}
+		s->stack_b[x] = 0;
+		x = get_relative_size(s->stack_a, s->size);
+		while (x > 0)
+		{
+			s->stack_a[x] = s->stack_a[x - 1];
+			x--;
+		}
+		s->stack_a[0] = aux;
+		save_next_move(s, "pa");
 	}
-	s->stack_b[x] = 0;
-	x = get_relative_size(s->stack_a, s->size);
-	while (x > 0)
-	{
-		s->stack_a[x] = s->stack_a[x - 1];
-		x--;
-	}
-	s->stack_a[0] = aux;
-	save_next_move(s, "pa");
 }
 
 void	action_pb(t_stacks *s)
@@ -40,22 +43,25 @@ void	action_pb(t_stacks *s)
 	int	x;
 	int	aux;
 
-	x = 0;
-	aux = s->stack_a[0];
-	while (x < get_relative_size(s->stack_a, s->size) - 1)
+	if (get_relative_size(s->stack_a, s->size) > 0)
 	{
-		s->stack_a[x] = s->stack_a[x + 1];
-		x++;
+		x = 0;
+		aux = s->stack_a[0];
+		while (x < get_relative_size(s->stack_a, s->size) - 1)
+		{
+			s->stack_a[x] = s->stack_a[x + 1];
+			x++;
+		}
+		s->stack_a[x] = 0;
+		x = get_relative_size(s->stack_b, s->size);
+		while (x > 0)
+		{
+			s->stack_b[x] = s->stack_b[x - 1];
+			x--;
+		}
+		s->stack_b[0] = aux;
+		save_next_move(s, "pb");
 	}
-	s->stack_a[x] = 0;
-	x = get_relative_size(s->stack_b, s->size);
-	while (x > 0)
-	{
-		s->stack_b[x] = s->stack_b[x - 1];
-		x--;
-	}
-	s->stack_b[0] = aux;
-	save_next_move(s, "pb");
 }
 
 void	action_rrx(int *stack, int *optional, t_stacks *s, char c)
@@ -63,22 +69,25 @@ void	action_rrx(int *stack, int *optional, t_stacks *s, char c)
 	int	x;
 	int	aux;
 
-	x = get_relative_size(stack, s->size) - 1;
-	aux = stack[x];
-	while (x > 0)
+	if (get_relative_size(stack, s->size) > 1)
 	{
-		stack[x] = stack[x - 1];
-		x--;
+		x = get_relative_size(stack, s->size) - 1;
+		aux = stack[x];
+		while (x > 0)
+		{
+			stack[x] = stack[x - 1];
+			x--;
+		}
+		stack[x] = aux;
+		if (optional != NULL)
+			action_rx(optional, NULL, s, 'x');
+		if (c == 'a')
+			save_next_move(s, "rra");
+		else if (c == 'b')
+			save_next_move(s, "rrb");
+		else if (c == 's')
+			save_next_move(s, "rrr");
 	}
-	stack[x] = aux;
-	if (optional != NULL)
-		action_rx(optional, NULL, s, 'x');
-	if (c == 'a')
-		save_next_move(s, "rra");
-	else if (c == 'b')
-		save_next_move(s, "rrb");
-	else if (c == 's')
-		save_next_move(s, "rrr");
 }
 
 void	action_rx(int *stack, int *optional, t_stacks *s, char c)
@@ -86,37 +95,43 @@ void	action_rx(int *stack, int *optional, t_stacks *s, char c)
 	int	x;
 	int	aux;
 
-	x = 0;
-	aux = stack[0];
-	while (x < get_relative_size(stack, s->size) - 1)
+	if (get_relative_size(stack, s->size) > 1)
 	{
-		stack[x] = stack[x + 1];
-		x++;
+		x = 0;
+		aux = stack[0];
+		while (x < get_relative_size(stack, s->size) - 1)
+		{
+			stack[x] = stack[x + 1];
+			x++;
+		}
+		stack[x] = aux;
+		if (optional != NULL)
+			action_rx(optional, NULL, s, 'x');
+		if (c == 'a')
+			save_next_move(s, "ra");
+		else if (c == 'b')
+			save_next_move(s, "rb");
+		else if (c == 's')
+			save_next_move(s, "rr");
 	}
-	stack[x] = aux;
-	if (optional != NULL)
-		action_rx(optional, NULL, s, 'x');
-	if (c == 'a')
-		save_next_move(s, "ra");
-	else if (c == 'b')
-		save_next_move(s, "rb");
-	else if (c == 's')
-		save_next_move(s, "rr");
 }
 
 void	action_sx(int *stack, int *optional, t_stacks *s, char c)
 {
 	int	aux;
 
-	aux = stack[0];
-	stack[0] = stack[1];
-	stack[1] = aux;
-	if (optional != NULL)
-		action_sx(optional, NULL, s, 'x');
-	if (c == 'a')
-		save_next_move(s, "sa");
-	else if (c == 'b')
-		save_next_move(s, "sb");
-	else if (c == 's')
-		save_next_move(s, "ss");
+	if (get_relative_size(stack, s->size) > 1)
+	{
+		aux = stack[0];
+		stack[0] = stack[1];
+		stack[1] = aux;
+		if (optional != NULL)
+			action_sx(optional, NULL, s, 'x');
+		if (c == 'a')
+			save_next_move(s, "sa");
+		else if (c == 'b')
+			save_next_move(s, "sb");
+		else if (c == 's')
+			save_next_move(s, "ss");
+	}
 }
--- src/fake_actions.c
+++ src/fake_actions.c
@@ -6,7 +6,7 @@
 /*   By: rfelipe- <rfelipe-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 17:43:29 by rfelipe-          #+#    #+#             */
-/*   Updated: 2021/11/12 17:53:05 by rfelipe-         ###   ########.fr       */
+/*   Updated: 2022/04/03 00:09:33 by rfelipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,26 @@ int	fake_action_pa(t_stacks *s)
 	int	x;
 	int	aux;
 
-	x = 0;
-	aux = s->stack_b[0];
-	while (x < get_relative_size(s->stack_b, s->size) - 1)
+	if (get_relative_size(s->stack_b, s->size) > 0)
 	{
-		s->stack_b[x] = s->stack_b[x + 1];
-		x++;
+		x = 0;
+		aux = s->stack_b[0];
+		while (x < get_relative_size(s->stack_b, s->size) - 1)
+		{
+			s->stack_b[x] = s->stack_b[x + 1];
+			x++;
+		}
+		s->stack_b[x] = 0;
+		x = get_relative_size(s->stack_a, s->size);
+		while (x > 0)
+		{
+			s->stack_a[x] = s->stack_a[x - 1];
+			x--;
+		}
+		s->stack_a[0] = aux;
+		return (1);
 	}
-	s->stack_b[x] = 0;
-	x = get_relative_size(s->stack_a, s->size);
-	while (x > 0)
-	{
-		s->stack_a[x] = s->stack_a[x - 1];
-		x--;
-	}
-	s->stack_a[0] = aux;
-	return (1);
+	return (0);
 }
 
 int	fake_action_pb(t_stacks *s)
@@ -40,22 +44,26 @@ int	fake_action_pb(t_stacks *s)
 	int	x;
 	int	aux;
 
-	x = 0;
-	aux = s->stack_a[0];
-	while (x < get_relative_size(s->stack_a, s->size) - 1)
+	if (get_relative_size(s->stack_a, s->size) > 0)
 	{
-		s->stack_a[x] = s->stack_a[x + 1];
-		x++;
+		x = 0;
+		aux = s->stack_a[0];
+		while (x < get_relative_size(s->stack_a, s->size) - 1)
+		{
+			s->stack_a[x] = s->stack_a[x + 1];
+			x++;
+		}
+		s->stack_a[x] = 0;
+		x = get_relative_size(s->stack_b, s->size);
+		while (x > 0)
+		{
+			s->stack_b[x] = s->stack_b[x - 1];
+			x--;
+		}
+		s->stack_b[0] = aux;
+		return (1);
 	}
-	s->stack_a[x] = 0;
-	x = get_relative_size(s->stack_b, s->size);
-	while (x > 0)
-	{
-		s->stack_b[x] = s->stack_b[x - 1];
-		x--;
-	}
-	s->stack_b[0] = aux;
-	return (1);
+	return (0);
 }
 
 int	fake_action_rrx(int *stack, int *optional, t_stacks *s)
@@ -64,19 +72,23 @@ int	fake_action_rrx(int *stack, int *optional, t_stacks *s)
 	int	aux;
 	int	moves;
 
-	x = get_relative_size(stack, s->size) - 1;
-	aux = stack[x];
-	moves = 0;
-	while (x > 0)
+	if (get_relative_size(stack, s->size) > 1)
 	{
-		stack[x] = stack[x - 1];
-		x--;
+		x = get_relative_size(stack, s->size) - 1;
+		aux = stack[x];
+		moves = 0;
+		while (x > 0)
+		{
+			stack[x] = stack[x - 1];
+			x--;
+		}
+		stack[x] = aux;
+		if (optional != NULL)
+			moves += fake_action_rx(optional, NULL, s);
+		moves++;
+		return (moves);
 	}
-	stack[x] = aux;
-	if (optional != NULL)
-		moves += fake_action_rx(optional, NULL, s);
-	moves++;
-	return (moves);
+	return (0);
 }
 
 int	fake_action_rx(int *stack, int *optional, t_stacks *s)
@@ -85,19 +97,23 @@ int	fake_action_rx(int *stack, int *optional, t_stacks *s)
 	int	aux;
 	int	moves;
 
-	x = 0;
-	aux = stack[0];
-	moves = 0;
-	while (x < get_relative_size(stack, s->size) - 1)
+	if (get_relative_size(stack, s->size) > 1)
 	{
-		stack[x] = stack[x + 1];
-		x++;
+		x = 0;
+		aux = stack[0];
+		moves = 0;
+		while (x < get_relative_size(stack, s->size) - 1)
+		{
+			stack[x] = stack[x + 1];
+			x++;
+		}
+		stack[x] = aux;
+		if (optional != NULL)
+			moves += fake_action_rx(optional, NULL, s);
+		moves++;
+		return (moves);
 	}
-	stack[x] = aux;
-	if (optional != NULL)
-		moves += fake_action_rx(optional, NULL, s);
-	moves++;
-	return (moves);
+	return (0);
 }
 
 int	fake_action_sx(int *stack, int *optional, t_stacks *s)
@@ -105,12 +121,16 @@ int	fake_action_sx(int *stack, int *optional, t_stacks *s)
 	int	aux;
 	int	moves;
 
-	moves = 0;
-	aux = stack[0];
-	stack[0] = stack[1];
-	stack[1] = aux;
-	if (optional != NULL)
-		moves += fake_action_sx(optional, NULL, s);
-	moves++;
-	return (moves);
+	if (get_relative_size(stack, s->size) > 1)
+	{
+		moves = 0;
+		aux = stack[0];
+		stack[0] = stack[1];
+		stack[1] = aux;
+		if (optional != NULL)
+			moves += fake_action_sx(optional, NULL, s);
+		moves++;
+		return (moves);
+	}
+	return (0);
 }
